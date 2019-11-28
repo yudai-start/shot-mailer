636630356cfbc7b7426dfa1314da8bc07ddd0f27
yudai <fukuta.yudai@gmail.com>
2019-11-28 15:49:20 +0900

    stars-shot-mailer

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,31 @@
+class PostsController < ApplicationController
+  
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    Post.create(post_params)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def post_params
+    params.permit(:content)
+  end
+end
